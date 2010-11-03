eaf1e254d59ecabf9cd98176c0c908caa2f3304d
Tomas Jakl <jaklt@u-pl14.ms.mff.cuni.cz>
2010-11-03 13:29:58 +0100

    Inline after function + optimize bits

--- MyBits.hs
+++ MyBits.hs
@@ -3,16 +3,14 @@ module MyBits (bitIndex, bitCount, rightMostBit, bits) where
 
 import Data.Bits ((.&.), xor)
 import Data.Int  (Int64)
--- import Foreign
--- import Foreign.C.Types
 
 foreign import ccall "clib.h bit_index" bitIndex :: Int64 -> Int
 foreign import ccall "clib.h bit_count" bitCount :: Int64 -> Int
 
-{-# INLINE rightMostBit #-}
 rightMostBit :: Int64 -> Int64
 rightMostBit n = n .&. (-n)
+{-# INLINE rightMostBit #-}
 
 bits :: Int64 -> [Int64]
 bits 0 = []
-bits n = rmb : bits (n `xor` rmb) where rmb = rightMostBit n
+bits n = rightMostBit n : bits (n .&. (n-1))
